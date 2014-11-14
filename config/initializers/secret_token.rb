c97db998f3886094fa1fd324f170e7addf8972c1
cblant20 <caila.blanton@careerbuilder.com>
2014-11-14 13:14:36 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ALarksNest::Application.config.secret_key_base = '8c7115c48afd97d57c31e03ce2c6afabdd1196501ac927c994d17c86fe3f39fb2df701527d0adada604d3fec1a821f90a8eaec020599478b2a4bc16fd42cea53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ALarksNest::Application.config.session_store :cookie_store, key: '_a-larks-nest_session'
